861fda47d09fdcf64f83c8cc903520ad8e88c79a
Jason Rhubottom <jason@rhusoft.com>
2020-06-17 21:02:38 -0700

    Removed unused modules

--- autoremovetorrents/client/transmission.py
+++ autoremovetorrents/client/transmission.py
@@ -1,6 +1,5 @@
 #-*- coding:utf-8 -*-
 import requests
-import array
 from ..torrent import Torrent
 from ..torrentstatus import TorrentStatus
 from ..exception.connectionfailure import ConnectionFailure
